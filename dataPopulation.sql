3310aaf3dc1a66dfaf9724f67bb2294d18e7adf2
ja-der <129236631+ja-der@users.noreply.github.com>
2024-04-01 02:40:41 -0400

    updated sql queries

---
+++ dataPopulation.sql
@@ -0,0 +1,207 @@
+
+-- POPULATING DATABASE SHOULD BE THE LAST THING THAT IS IMPLEMENTED
+-- basically complete everything else in the project first, then populate the database with data
+-- changing the fields is very difficult once the data is populated
+-- very time consuming to change the fields and the data
+
+/*
+--------------------------------------------------------------
+Chain 1
+--------------------------------------------------------------
+*/
+-- Insert data for Chain 1
+INSERT INTO Chain (ChainName, HeadquartersAddress, NumberOfHotels, HeadquartersEmail, HeadquartersPhoneNumber) 
+VALUES ('Chain 1', '123 Main Street, City1, Countryhq1@example.com', '+1231', 8, '4567890');
+
+-- Insert data for Chain 1 Hotels
+INSERT INTO Hotel (HotelAddress, HotelPhoneNumber, HotelEmail, StarRating, NumberOfRooms, ChainID)
+VALUES 
+    ('123 Hotel Ave, City1, Country1', '+1234567890', 'hotel1@example.com', 4, 50, 1), -- Hotel 1
+    ('456 Resort St, City2, Country1', '+1234567890', 'resort1@example.com', 5, 100, 1), -- Hotel 2
+    ('789 Lodge Blvd, City3, Country2', '+1234567890', 'lodge1@example.com', 3, 80, 1), -- Hotel 3
+    ('987 Inn Ln, City4, Country2', '+1234567890', 'inn1@example.com', 4, 120, 1), -- Hotel 4
+    ('543 Motel Rd, City5, Country3', '+1234567890', 'motel1@example.com', 2, 60, 1), -- Hotel 5
+    ('654 Plaza Dr, City6, Country3', '+1234567890', 'plaza1@example.com', 3, 90, 1), -- Hotel 6
+    ('876 Villa Ave, City7, Country4', '+1234567890', 'villa1@example.com', 4, 110, 1), -- Hotel 7
+    ('210 Retreat Blvd, City8, Country4', '+1234567890', 'retreat1@example.com', 5, 150, 1); -- Hotel 8
+
+-- Insert data for rooms in each hotel
+-- Hotel 1 Rooms
+INSERT INTO Room (Price, Amenities, Capacity, View, Extendable, Issues, HotelID, ChainID)
+VALUES
+   -- hotel 1
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 1, 1), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 1, 1), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 1, 1), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 1, 1), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 1, 1), -- Room 5
+    -- hotel 2
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 2, 1), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 2, 1), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 2, 1), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 2, 1), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 2, 1), -- Room 5
+    -- hotel 3
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 3, 1), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 3, 1), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 3, 1), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 3, 1), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 3, 1), -- Room 5
+    -- hotel 4
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 4, 1), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 4, 1), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 4, 1), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 4, 1), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 4, 1), -- Room 5
+    -- hotel 5
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 5, 1), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 5, 1), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 5, 1), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 5, 1), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 5, 1), -- Room 5
+    -- hotel 6
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 6, 1), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 6, 1), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 5, 1), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 6, 1), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 6, 1), -- Room 5
+    -- hotel 7
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 7, 1), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 7, 1), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 7, 1), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 7, 1), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 7, 1), -- Room 5
+    -- hotel 8
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 8, 1), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 8, 1), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 8, 1), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 8, 1), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 8, 1) -- Room 5
+;
+
+-- Insert data for Clients
+INSERT INTO Client (ClientFirstName, ClientLastName, ClientAddress, ClientSSN, RegistrationDate, ClientEmail, ClientPassword)
+VALUES 
+    ('John', 'Doe', '123 Client St, City1, Country1', 123456789, '2024-03-26', 'johndoe@gmail.com', 'password123'),
+    ('Jane', 'Smith', '456 Client Ave, City2, Country1', 987654321, '2024-03-27', 'janesmith@gmail.com', 'password123')
+;
+
+-- Insert data for Employees
+INSERT INTO Employee (EmployeeFirstName, EmployeeLastName, EmployeeAddress, EmployeeSSN, EmployeeRole, HotelID, ChainID)
+VALUES 
+    ('Alice', 'Johnson', '123 Employee St, City1, Country1', 111111111, 'Manager', 1, 1),
+    ('Bob', 'Smith', '456 Employee Ave, City2, Country1', 222222222, 'Front Desk Clerk', 1, 1)
+    
+;
+
+-- Insert data for Reservations
+INSERT INTO Reservation (CheckInDate, CheckOutDate, ClientID, HotelID, RoomID)
+VALUES 
+    ('2024-04-01', '2024-04-05', 1, 1, 1), -- Client John Doe reserves Hotel 1 Room 1
+    ('2024-04-02', '2024-04-06', 2, 2, 2) -- Client Jane Smith reserves Hotel 2 Room 2
+;
+
+-- Insert data for Positions
+INSERT INTO Position (JobTitle, Responsibilities, Level, EmployeeID)
+VALUES 
+    ('Manager', 'Oversee hotel operations', 1, 1), -- Manager position at Hotel 1
+    ('Front Desk Clerk', 'Assist guests with check-in and check-out', 2, 1) -- Front Desk Clerk position at Hotel 1
+;
+
+/*
+--------------------------------------------------------------
+Chain 2
+--------------------------------------------------------------
+*/
+
+-- Insert data for Chain 2
+INSERT INTO Chain (ChainName, HeadquartersAddress, NumberOfHotels, HeadquartersEmail, HeadquartersPhoneNumber) 
+VALUES ('Chain 2', '456 Elm Street, City2, Country2', 8, 'hq2@example.com', '+1234567890');
+
+-- Insert data for Chain 2 Hotels
+INSERT INTO Hotel (HotelAddress, HotelPhoneNumber, HotelEmail, StarRating, NumberOfRooms, ChainID)
+VALUES 
+    ('123 Resort Ave, City1, Country2', '+1234567890', 'resort2@example.com', 4, 50, 2), -- Hotel 1
+    ('456 Lodge St, City2, Country2', '+1234567890', 'lodge2@example.com', 5, 100, 2), -- Hotel 2
+    ('789 Inn Blvd, City3, Country2', '+1234567890', 'inn2@example.com', 3, 80, 2), -- Hotel 3
+    ('987 Motel Ln, City4, Country2', '+1234567890', 'motel2@example.com', 4, 120, 2), -- Hotel 4
+    ('543 Plaza Rd, City5, Country2', '+1234567890', 'plaza2@example.com', 2, 60, 2), -- Hotel 5
+    ('654 Villa Dr, City6, Country2', '+1234567890', 'villa2@example.com', 3, 90, 2), -- Hotel 6
+    ('876 Retreat Ave, City7, Country2', '+1234567890', 'retreat2@example.com', 4, 110, 2), -- Hotel 7
+    ('210 Beach Blvd, City8, Country2', '+1234567890', 'beach2@example.com', 5, 150, 2); -- Hotel 8
+
+-- Insert data 5 rooms in each hotel in Chain 2
+-- Hotel 2 Rooms
+INSERT INTO Room (Price, Amenities, Capacity, View, Extendable, Issues, HotelID, ChainID)
+VALUES
+    -- hotel 1
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 1, 2), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 1, 2), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 1, 2), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 1, 2), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 1, 2), -- Room 5
+    -- hotel 2
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 2, 2), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 2, 2), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 2, 2), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 2, 2), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 2, 2), -- Room 5
+    -- hotel 3
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 3, 2), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 3, 2), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 3, 2), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 3, 2), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 3, 2), -- Room 5
+    -- hotel 4
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 4, 2), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 4, 2), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 4, 2), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 4, 2), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 4, 2), -- Room 5
+    -- hotel 5
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 5, 2), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 5, 2), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 5, 2), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 5, 2), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 5, 2), -- Room 5
+    -- hotel 6
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 6, 2), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 6, 2), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 5, 2), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 6, 2), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 6, 2), -- Room 5
+    -- hotel 7
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 7, 2), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 7, 2), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 7, 2), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 7, 2), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 7, 2), -- Room 5
+    -- hotel 8
+    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 8, 2), -- Room 1
+    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 8, 2), -- Room 2
+    (120.00, 'Wi-Fi, TV, AC', 5, 'City view', 'Yes', NULL, 8, 2), -- Room 3
+    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 8, 2), -- Room 4
+    (180.00, 'Wi-Fi, TV, AC, Balcony', 1, 'City view', 'Yes', NULL, 8, 2) -- Room 5
+;
+
+
+-- Insert data for Clients for Chain 2
+INSERT INTO Client (ClientFirstName, ClientLastName, ClientAddress, ClientSSN, RegistrationDate, ClientEmail, ClientPassword)
+VALUES 
+    ('Michael', 'Brown', '123 Client St, City1, Country2', 111111111, '2024-03-28', 'michael@gmail.com', 'password123'),
+    ('Jennifer', 'Davis', '456 Client Ave, City2, Country2', 222222222, '2024-03-29', 'davis@gmail.com', 'password123')
+;
+
+-- Insert data for Employees for Chain 2
+INSERT INTO Employee (EmployeeFirstName, EmployeeLastName, EmployeeAddress, EmployeeSSN, EmployeeRole, HotelID, ChainID)
+VALUES 
+    ('David', 'Wilson', '123 Employee St, City1, Country2', 333333333, 'Manager', 1, 2),
+    ('Jessica', 'Martinez', '456 Employee Ave, City2, Country2', 444444444, 'Front Desk Clerk', 1, 2)
+;
+
+-- Insert data for Reservations for Chain 2
+INSERT INTO Reservation (CheckInDate, CheckOutDate, ClientID, HotelID, RoomID)
+VALUES 
+    ('2024-04-03', '2024-04-07', 1, 1, 1), -- Client Michael Brown reserves Hotel 1 Room 1
+    ('2024-04-04', '2024-04-08', 2, 2, 2) -- Client Jennifer Davis reserves Hotel 2 Room 2
+;
--- tobeimplemented.sql
+++ tobeimplemented.sql
@@ -1,7 +1,21 @@
 /*
 --------------------------------------------------------------
+Drop Table Queries
+For testing only
+--------------------------------------------------------------
+*/
+DROP TABLE Rental;
+DROP TABLE Position;
+DROP TABLE Employee;
+DROP TABLE Reservation;
+DROP TABLE Client;
+DROP TABLE Room;
+DROP TABLE Hotel;
+DROP TABLE Chain;
+
+/*
+--------------------------------------------------------------
 Table Creation Queries
-APPLY QUERIES IN ORDER (or else it won't work)
 --------------------------------------------------------------
 */
 CREATE TABLE Chain (
@@ -94,144 +108,62 @@ CREATE TABLE Position (
     JobTitle VARCHAR(255) PRIMARY KEY,
     Responsibilities VARCHAR(1000),
     Level INT,
-    HotelID INT NOT NULL,
-    ChainID INT NOT NULL,
-    FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID),
-    FOREIGN KEY (ChainID) REFERENCES Chain(ChainID)
+    EmployeeID INT NOT NULL,
+    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
 );
 
 
 /*
 --------------------------------------------------------------
-Random Data Pop Queries
+Queries (Au moins 4 Requetes) and Triggers (au moins 2)
 --------------------------------------------------------------
 */
 
--- Insert data for Chain 1
-INSERT INTO Chain (Name, HeadquartersAddress, NumberOfHotels, HeadquartersEmail, HeadquartersPhoneNumber) 
-VALUES ('Chain 1', '123 Main Street, City1, Country1', 8, 'hq1@example.com', '+1234567890');
-
--- Insert data for Chain 1 Hotels
-INSERT INTO Hotel (Address, PhoneNumber, Email, StarRating, NumberOfRooms, ChainID)
-VALUES 
-    ('123 Hotel Ave, City1, Country1', '+1234567890', 'hotel1@example.com', 4, 50, 1), -- Hotel 1
-    ('456 Resort St, City2, Country1', '+1234567890', 'resort1@example.com', 5, 100, 1), -- Hotel 2
-    ('789 Lodge Blvd, City3, Country1', '+1234567890', 'lodge1@example.com', 3, 80, 1), -- Hotel 3
-    ('987 Inn Ln, City4, Country1', '+1234567890', 'inn1@example.com', 4, 120, 1), -- Hotel 4
-    ('543 Motel Rd, City5, Country1', '+1234567890', 'motel1@example.com', 2, 60, 1), -- Hotel 5
-    ('654 Plaza Dr, City6, Country1', '+1234567890', 'plaza1@example.com', 3, 90, 1), -- Hotel 6
-    ('876 Villa Ave, City7, Country1', '+1234567890', 'villa1@example.com', 4, 110, 1), -- Hotel 7
-    ('210 Retreat Blvd, City8, Country1', '+1234567890', 'retreat1@example.com', 5, 150, 1); -- Hotel 8
-
--- Insert data for rooms in each hotel
--- Hotel 1 Rooms
-INSERT INTO Room (Price, Amenities, Capacity, View, Extendable, Issues, HotelID, ChainID)
-VALUES
-    (100.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 1, 1), -- Room 1
-    (150.00, 'Wi-Fi, TV, AC, Mini Fridge', 4, 'Ocean view', 'No', NULL, 1, 1), -- Room 2
-    (120.00, 'Wi-Fi, TV, AC', 2, 'City view', 'Yes', NULL, 1, 1), -- Room 3
-    (200.00, 'Wi-Fi, TV, AC, Kitchenette', 3, 'Mountain view', 'No', NULL, 1, 1), -- Room 4
-    (180.00, 'Wi-Fi, TV, AC, Balcony', 2, 'City view', 'Yes', NULL, 1, 1) -- Room 5
-;
-
--- Insert data for Clients
-INSERT INTO Client (FirstName, LastName, Address, SSN, RegistrationDate)
-VALUES 
-    ('John', 'Doe', '123 Client St, City1, Country1', 123456789, '2024-03-26'),
-    ('Jane', 'Smith', '456 Client Ave, City2, Country1', 987654321, '2024-03-27'),
-    -- Add more clients for Chain 1 as needed
-;
-
--- Insert data for Employees
-INSERT INTO Employee (FirstName, LastName, Address, SSN, Role, HotelID, ChainID)
-VALUES 
-    ('Alice', 'Johnson', '123 Employee St, City1, Country1', 111111111, 'Manager', 1, 1),
-    ('Bob', 'Smith', '456 Employee Ave, City2, Country1', 222222222, 'Front Desk Clerk', 1, 1),
-    -- Add more employees for Chain 1 as needed
-;
-
--- Insert data for Reservations
-INSERT INTO Reservation (CheckInDate, CheckOutDate, ClientID, HotelID, RoomID)
-VALUES 
-    ('2024-04-01', '2024-04-05', 1, 1, 1), -- Client John Doe reserves Hotel 1 Room 1
-    ('2024-04-02', '2024-04-06', 2, 2, 2), -- Client Jane Smith reserves Hotel 2 Room 2
-    -- Add more reservations for Chain 1 as needed
-;
-
--- Insert data for Positions
-INSERT INTO Position (JobTitle, Responsibilities, Level, HotelID)
-VALUES 
-    ('Manager', 'Oversee hotel operations', 3, 1), -- Manager position at Hotel 1
-    ('Front Desk Clerk', 'Assist guests with check-in and check-out', 1, 1), -- Front Desk Clerk position at Hotel 1
-    -- Add more positions for Chain 1 as needed
-;
-
-
--- Insert data for Chain 2
-INSERT INTO Chain (Name, HeadquartersAddress, NumberOfHotels, HeadquartersEmail, HeadquartersPhoneNumber) 
-VALUES ('Chain 2', '456 Elm Street, City2, Country2', 8, 'hq2@example.com', '+1234567890');
-
--- Insert data for Chain 2 Hotels
-INSERT INTO Hotel (Address, PhoneNumber, Email, StarRating, NumberOfRooms, ChainID)
-VALUES 
-    ('123 Resort Ave, City1, Country2', '+1234567890', 'resort2@example.com', 4, 50, 2), -- Hotel 1
-    ('456 Lodge St, City2, Country2', '+1234567890', 'lodge2@example.com', 5, 100, 2), -- Hotel 2
-    ('789 Inn Blvd, City3, Country2', '+1234567890', 'inn2@example.com', 3, 80, 2), -- Hotel 3
-    ('987 Motel Ln, City4, Country2', '+1234567890', 'motel2@example.com', 4, 120, 2), -- Hotel 4
-    ('543 Plaza Rd, City5, Country2', '+1234567890', 'plaza2@example.com', 2, 60, 2), -- Hotel 5
-    ('654 Villa Dr, City6, Country2', '+1234567890', 'villa2@example.com', 3, 90, 2), -- Hotel 6
-    ('876 Retreat Ave, City7, Country2', '+1234567890', 'retreat2@example.com', 4, 110, 2), -- Hotel 7
-    ('210 Beach Blvd, City8, Country2', '+1234567890', 'beach2@example.com', 5, 150, 2); -- Hotel 8
-
--- Insert data for rooms in each hotel in Chain 2
--- Repeat the process similarly to Chain 1 for each hotel in Chain 2
-
--- Insert data for Clients for Chain 2
-INSERT INTO Client (FirstName, LastName, Address, SSN, RegistrationDate)
-VALUES 
-    ('Michael', 'Brown', '123 Client St, City1, Country2', 111111111, '2024-03-28'),
-    ('Jennifer', 'Davis', '456 Client Ave, City2, Country2', 222222222, '2024-03-29'),
-    -- Add more clients for Chain 2 as needed
-;
-
--- Insert data for Employees for Chain 2
-INSERT INTO Employee (FirstName, LastName, Address, SSN, Role, HotelID, ChainID)
-VALUES 
-    ('David', 'Wilson', '123 Employee St, City1, Country2', 333333333, 'Manager', 1, 2),
-    ('Jessica', 'Martinez', '456 Employee Ave, City2, Country2', 444444444, 'Front Desk Clerk', 1, 2),
-    -- Add more employees for Chain 2 as needed
-;
-
--- Insert data for Reservations for Chain 2
-INSERT INTO Reservation (CheckInDate, CheckOutDate, ClientID, HotelID, RoomID)
-VALUES 
-    ('2024-04-03', '2024-04-07', 1, 1, 1), -- Client Michael Brown reserves Hotel 1 Room 1
-    ('2024-04-04', '2024-04-08', 2, 2, 2), -- Client Jennifer Davis reserves Hotel 2 Room 2
-    -- Add more reservations for Chain 2 as needed
-;
-
--- Insert data for Positions for Chain 2
-INSERT INTO Position (JobTitle, Responsibilities, Level, HotelID)
-VALUES 
-    ('Manager', 'Oversee hotel operations', 3, 1), -- Manager position at Hotel 1
-    ('Front Desk Clerk', 'Assist guests with check-in and check-out', 1, 1), -- Front Desk Clerk position at Hotel 1
-    -- Add more positions for Chain 2 as needed
-;
-
---- .... continue 
-
-
-/*
---------------------------------------------------------------
-Example Queries
---------------------------------------------------------------
-*/
+-- Inserting a reservation
 INSERT INTO Reservation (CheckInDate, CheckOutDate, ClientID, HotelID, RoomID)
 VALUES ('2024-04-20', '2024-04-25', 1, 2, 3);
 
-UPDATE Room SET Issues = 'Water leak' WHERE RoomID = 3;
+-- Trigger before inserting a new reservation to check room availability 
+CREATE OR REPLACE FUNCTION check_room_availability()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    IF (SELECT COUNT(*) FROM Reservation WHERE RoomID = NEW.RoomID AND (CheckInDate <= NEW.CheckOutDate AND CheckOutDate >= NEW.CheckInDate)) > 0 THEN
+        RAISE EXCEPTION 'Room not available for specified dates';
+    END IF;
+    RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+CREATE TRIGGER before_reservation_insert
+BEFORE INSERT ON Reservation
+FOR EACH ROW
+EXECUTE FUNCTION check_room_availability();
 
-DELETE FROM Chain WHERE ChainID = 1;
+
+-- Deleting a client and all associated reservations
+DELETE FROM Client WHERE ClientID = 1;
+
+-- Trigger after deleting a client to delete all associated reservations
+CREATE OR REPLACE FUNCTION delete_related_reservations()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    DELETE FROM Reservation WHERE ClientID = OLD.ClientID;
+    RETURN OLD;
+END;
+$$
+LANGUAGE plpgsql;
+CREATE TRIGGER after_client_delete
+AFTER DELETE ON Client
+FOR EACH ROW
+EXECUTE FUNCTION delete_related_reservations();
+
+--Updating the address of a hotel:
+UPDATE Hotel SET HotelAddress = 'New Address' WHERE HotelID = 1;
+
+-- Finding all reservations for a specific client
+SELECT * FROM Reservation WHERE ClientID = 1;
 
 
 /*
@@ -239,45 +171,41 @@ DELETE FROM Chain WHERE ChainID = 1;
 Indexes
 --------------------------------------------------------------
 */
--- On Reservation.RoomID to speed up room availability searches:
-CREATE INDEX idx_reservation_roomid ON Reservation(RoomID);
+-- Used for indexing by room price to speed up searches for rooms within a specific price range:
+CREATE INDEX idx_room_price ON Room(Price);
+-- Expected Queries: Queries that involve checking room price and filtering reservations by price
+-- Expected Data Updates: Updates to room data that may affect pricing, such as inserting or updating rooms.
 
--- On Hotel.ChainID to optimize queries filtering hotels by their chain:
+-- Used for Hotel.ChainID to optimize queries filtering hotels by their chain:
 CREATE INDEX idx_hotel_chainid ON Hotel(ChainID);
+-- Expected Queries: Queries that involve filtering hotels by their chain, retrieving hotels associated with a particular chain, or identifying chains that own specific hotels.
+-- Expected Data Updates: Updates to hotel data that may involve changes in the chain association, such as reassigning hotels to different chains or adding new hotels to existing chains.
 
--- On Client.LastName and Client.FirstName to improve search performance for clients by name:
+---- On Client.LastName and Client.FirstName to improve search performance for clients by name:
 CREATE INDEX idx_client_lastname_firstname ON Client(LastName, FirstName);
-
+-- Expected Queries: Queries that involve searching for clients by their last name, first name, or both, such as retrieving client information for bookings or filtering clients for specific marketing campaigns.
+-- Expected Data Updates: Updates or adding to client data that may involve changes in the last name or first name
 
 /*
 --------------------------------------------------------------
-Triggers
+Views
 --------------------------------------------------------------
 */
+--Number of Available Rooms per City
+-- Problem is what do they mean by zone, and if they do mean city, may need to change column in Hotel table
+CREATE VIEW AvailableRoomsPerCity AS
+SELECT h.HotelAddress AS City,
+       COUNT(r.RoomID) AS AvailableRooms
+FROM Hotel h
+LEFT JOIN Room r ON h.HotelID = r.HotelID
+WHERE r.RoomID NOT IN (SELECT Reservation.RoomID FROM Reservation)
+GROUP BY h.HotelAddress;
 
--- Before deleting a hotel chain, ensure no hotels are linked to this chain:
-CREATE OR REPLACE FUNCTION prevent_chain_deletion()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF (SELECT count(*) FROM Hotel WHERE ChainID = OLD.ChainID) > 0 THEN
-        RAISE EXCEPTION 'Cannot delete chain % because it still has hotels.', OLD.Name;
-    END IF;
-    RETURN OLD;
-END;
-$$ LANGUAGE plpgsql;
-CREATE TRIGGER BeforeChainDeletion
-BEFORE DELETE ON Chain
-FOR EACH ROW EXECUTE FUNCTION prevent_chain_deletion();
-
--- After inserting a reservation, verify room capacity and adjust the available number of rooms accordingly:1
--- Assuming you have a column or logic to manage availability, this trigger is conceptual
-CREATE OR REPLACE FUNCTION update_room_availability()
-RETURNS TRIGGER AS $$
-BEGIN
-    UPDATE Hotel SET NumberOfRooms = NumberOfRooms - 1 WHERE HotelID = NEW.HotelID;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-CREATE TRIGGER AfterReservationInsertion
-AFTER INSERT ON Reservation
-FOR EACH ROW EXECUTE FUNCTION update_room_availability();
+--Capacity of All Rooms in a Specific Hotel
+CREATE VIEW RoomCapacityPerHotel AS
+SELECT h.HotelID,
+       h.HotelName,
+       SUM(r.Capacity) AS TotalCapacity
+FROM Hotel h
+JOIN Room r ON h.HotelID = r.HotelID
+GROUP BY h.HotelID, h.HotelName;
