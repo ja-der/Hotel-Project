63a44f3a036705ca9264b3cd7bb1b20d6ff5f7a5
ja-der <129236631+ja-der@users.noreply.github.com>
2024-04-03 17:53:00 -0400

    Revert "Prevent unauthorized check-in"
    This reverts commit dddada7206813fe66c39d93cfe24a504196a932c.

--- frontend/src/components/employee/ReservationCheck.js
+++ frontend/src/components/employee/ReservationCheck.js
@@ -58,7 +58,7 @@ const ReservationCheck = ( ) => {
             if (parseRes.rentalid) {
                 toast.success("Reservation checked in successfully!");
             } else {
-                toast.error(parseRes);
+                toast.error("Reservation check-in failed. Please try again.");
             }
 
         } catch (err) {
