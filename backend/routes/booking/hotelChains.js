2c2fdda203c68d204b793cc9f27dae61c681a29f
ja-der <129236631+ja-der@users.noreply.github.com>
2024-04-05 23:30:41 -0400

    cleanup some unused code

--- backend/routes/booking/hotelChains.js
+++ backend/routes/booking/hotelChains.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const pool = require('../../db');
+// const router = require('express').Router();
+// const pool = require('../../db');
 
-router.get('/', async (req, res) => {
-  try {
-    const queryResult = await pool.query('SELECT chainid, chainname FROM chain');
-    res.json(queryResult.rows);
-  } catch (err) {
-    console.error('Error querying hotel chains:', err);
-    res.status(500).send('Server error');
-  }
-});
-module.exports = router;
+// router.get('/', async (req, res) => {
+//   try {
+//     const queryResult = await pool.query('SELECT chainid, chainname FROM chain');
+//     res.json(queryResult.rows);
+//   } catch (err) {
+//     console.error('Error querying hotel chains:', err);
+//     res.status(500).send('Server error');
+//   }
+// });
+// module.exports = router;
--- backend/routes/booking/hotelCities.js
+++ backend/routes/booking/hotelCities.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const pool = require('../../db');
+// const router = require('express').Router();
+// const pool = require('../../db');
 
-router.get('/', async (req, res) => {
-  try {
-    const queryResult = await pool.query('SELECT hotelid, hotelcity FROM hotel');
-    res.json(queryResult.rows);
-  } catch (err) {
-    console.error('Error querying cities:', err);
-    res.status(500).send('Server error');
-  }
-});
-module.exports = router;
+// router.get('/', async (req, res) => {
+//   try {
+//     const queryResult = await pool.query('SELECT hotelid, hotelcity FROM hotel');
+//     res.json(queryResult.rows);
+//   } catch (err) {
+//     console.error('Error querying cities:', err);
+//     res.status(500).send('Server error');
+//   }
+// });
+// module.exports = router;
